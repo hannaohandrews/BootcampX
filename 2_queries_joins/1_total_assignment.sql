fa3faf5478e4b6f3abdb35cb21c19702d7797470
hannaohandrews <hannaohandrews@gmail.com>
2020-07-21 03:10:42 +0000

    2_queries

---
+++ 2_queries_joins/1_total_assignment.sql
@@ -0,0 +1,7 @@
+SELECT sum(assignment_submissions.duration) as total_duration
+FROM students
+JOIN assignment_submissions ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = 'FEB 12'
+
+
