079792784f2bf9d97b7fc4cdc66e1b00a3289261
hannaohandrews <hannaohandrews@gmail.com>
2020-07-23 19:55:26 +0000

    sql Injection proof

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -13,16 +13,22 @@ pool.connect();
 //   .then((result) => console.log(result));
 
 
+const queryString = `
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE $1
+;
+`
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`]
+
 pool
-  .query(`
-  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-  FROM students
-  JOIN cohorts ON cohorts.id = cohort_id
-  WHERE cohorts.name LIKE '%${process.argv[2]}%'
-  LIMIT ${process.argv[3] || 5};
-  `)
+  .query(queryString,values)
   .then(res => {
-    res.rows.forEach(user => {
-      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    res.rows.forEach(row => {
+      // console.log(row)
+      console.log(`${row.name} has an id of ${row.student_id} and was in the ${row.cohort} cohort`);
     })
   }).catch(err => console.error('query error', err.stack));
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,26 +8,30 @@ const pool = new Pool({
 });
 
 pool.connect();
-// pool
-//   .query('SELECT * FROM students')
-//   .then((result) => console.log(result));
 
 
-pool
-.query(`
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`]
+
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+`
+
+pool.query(queryString,values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
   })
-});
+})
+.catch(err => console.error('query error', err.stack));
+
 
 
 
